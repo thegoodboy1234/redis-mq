99f82307cbc440c0a4d7e748662f2c81b5dcd8dd
je08@jingshukj.com.cn <je08@jingshukj.com.cn>
2019-11-21 19:52:04 +0800

    init

---
+++ src/Mq.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jskj
+ * Date: 2019/11/21
+ * Time: 16:28
+ */
+
+namespace RedisMq;
+
+
+class Mq
+{
+    public function run()
+    {
+        echo 123;
+    }
+}
